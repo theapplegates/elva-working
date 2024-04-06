c7a199fffd5db080f493180f0fe4c4a7785de5da
Paul <paul@theapplegates.com>
2024-04-06 16:27:24 -0400

    first commit

---
+++ src/_data/navigation.js
@@ -0,0 +1,35 @@
+module.exports = {
+    en: [
+		{
+			text: 'Search',
+			url: '/search/'
+		},
+		{
+			text: 'About',
+			url: '/about/'
+		},
+		{
+			text: 'Documentation',
+			url: '/'
+		},
+		{
+			text: 'GitHub',
+			url: 'https://github.com/scottsweb/elva'
+		}
+    ],
+    sv: [
+		{
+			text: 'Om',
+			url: '/om/'
+		},
+		{
+			text: 'Dokumentation',
+			url: '/'
+		},
+		{
+			text: 'GitHub',
+			url: 'https://github.com/scottsweb/elva'
+		}
+    ]
+};
+  
